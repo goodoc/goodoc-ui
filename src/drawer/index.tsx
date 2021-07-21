c5da2d93e7e53fa409326d655d93ba01b85eae0a
dany <dayong7387@gmail.com>
2021-07-21 16:58:31 +0900

    fix(drawer): drawer 라이프사이클 / z-index 삭제

--- src/drawer/index.tsx
+++ src/drawer/index.tsx
@@ -10,22 +10,18 @@ type ModalProps = {
 
 export default function Drawer({ children, close, isOpen }: ModalProps) {
   const width = typeof window === 'undefined' ? '100%' : window.innerWidth
-  const [isShow, setShow] = useState(false);
 
-  useEffect(() => {
-    setShow(isOpen);
-  }, [isOpen])
   return (
     <>
       <div
         style={{
           width: width,
-          zIndex: isOpen ? 9999999999999 : -1,
+          zIndex: 9999999999999,
           borderTopLeftRadius: '8px',
           borderTopRightRadius: '8px',
           backgroundColor: '#ffffff',
         }}
-        className={isShow ? 'drawer isOpen' : 'drawer'}>
+        className={isOpen ? 'drawer isOpen' : 'drawer'}>
         <div
           style={{
             margin: '40px 20px 12px 20px'
